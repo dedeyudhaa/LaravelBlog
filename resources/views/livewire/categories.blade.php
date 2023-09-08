9ec009f17cdf26c0c3c39a9db3811a372b4891ef
dedeyudhaa <dedeyudhaa12@gmail.com>
2023-09-08 17:02:30 +0700

    Display Categories & Sub Categories

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -76,22 +76,21 @@
                             </thead>
                             <tbody>
                                 @forelse ($subcategories as $subcategory)
-                                    
-                                
+                                                                    
                                 <tr>
-                                <td>{{ $subcategory->subcategory_name }}</td>
-                                <td class="text-muted">
-                                    {{ $subcategory->parentCategory->category_name }}
-                                </td>
-                                <td>
-                                    {{ $subcategory->posts->count() }}
-                                </td>
-                                <td>
-                                    <div class="btn-group">
-                                        <a href="#" class="btn btn-sm btn-primary" wire:click.prevent='editSubCategory({{ $subcategory->id }})'>Edit</a> &nbsp;
-                                        <a href="" class="btn btn-sm btn-danger" wire:click.prevent='deleteSubCategory({{ $subcategory->id }})'>Delete</a>
-                                    </div>
-                                </td>
+                                    <td>{{ $subcategory->subcategory_name }}</td>
+                                    <td class="text-muted">
+                                        {{ $subcategory->parent_category != 0 ? $subcategory->parentCategory->category_name : '-' }}
+                                    </td>
+                                    <td>
+                                        {{ $subcategory->posts->count() }}
+                                    </td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <a href="#" class="btn btn-sm btn-primary" wire:click.prevent='editSubCategory({{ $subcategory->id }})'>Edit</a> &nbsp;
+                                            <a href="" class="btn btn-sm btn-danger" wire:click.prevent='deleteSubCategory({{ $subcategory->id }})'>Delete</a>
+                                        </div>
+                                    </td>
                                 </tr>
 
                                 @empty
@@ -174,10 +173,9 @@
                     <div class="mb-3">
                         <div class="form-label">Parent Category</div>
                         <select class="form-select" wire:model='parent_category'>
-                            @if (!$updateSubCategoryMode)
-                                <option value="">No Selected</option>
-                            @endif
-
+                            
+                            <option value="0">-- Uncategorized --</option>
+                            
                             @foreach (\App\Models\Category::all() as $category)
                                 <option value="{{ $category->id }}">{{ $category->category_name }}</option>
                             @endforeach
